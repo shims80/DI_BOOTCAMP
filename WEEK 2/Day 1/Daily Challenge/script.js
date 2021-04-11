5013636504810c4c7e463895a33b355209cde74e
shims80 <shims80@gmail.com>
2021-04-11 16:12:31 +0300

    daily challenge

---
+++ WEEK 2/Day 1/Daily Challenge/script.js
@@ -0,0 +1,17 @@
+Exercise 1:
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits);
+console.log(fruits.sort());
+fruits.push("Kiwi");
+console.log(fruits);
+fruits = fruits.slice(1);
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+Exercise 2:
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0]);
+
+
---
+++ WEEK 2/Day 1/xp gold/script.js
@@ -0,0 +1,7 @@
+//Exercise 1 : Favorite Color
+// let me = ["my","favorite","color","is","blue"]
+// let color =me.join();
+// console.log(color);
+
+//Exercise 2 :
+
