57bc73f0381569764d4d57383a61680ad6f28610
shims80 <shims80@gmail.com>
2021-04-11 14:07:39 +0300

    adding

---
+++ WEEK 2/Day 1/Exrcise 1/script.js
@@ -0,0 +1,14 @@
+// 5Exrcise 1 :
+//    5 - "4" =534
+// 10 % 5 = 2
+// 5 % 10 = 0
+// "Java" + "Script" =javascript 
+// " " + " " ="   "
+// " " + 0  =" 0"
+// true + true = true
+// true + false= 1
+// false + true= 1
+// false - true= -1
+// 3 - 4=  -1
+// "Bob" - "bill" =NaN
+
