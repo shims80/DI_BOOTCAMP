3923e88d2aa6129609d32788c4349997e01c3bf5
shims80 <shims80@gmail.com>
2021-04-11 12:49:52 +0300

    new files

---
+++ WEEK 2/Day 1/script.js
@@ -0,0 +1,8 @@
+//Alert
+alert("Hello");
+//Prompt
+let age = prompt('How old are you?', );
+alert(`You are ${age} years old!`); // You are 20 years old!
+//Confirm
+let isBoss = confirm("Are you the boss?",);
+alert(isBoss); // true if OK is pressed
