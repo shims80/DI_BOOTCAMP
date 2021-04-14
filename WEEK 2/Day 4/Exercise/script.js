c2cfc4d55e726b7cc126d09c962a9470b61c5f95
shims80 <shims80@gmail.com>
2021-04-14 12:36:00 +0300

    Functions

---
+++ WEEK 2/Day 4/Exercise/script.js
@@ -0,0 +1,20 @@
+// // //class exrcise 1:
+// function userInfo() {
+//     myAge = prompt("what is your age?");
+//     ageOfMum = (myAge * 2);
+//     console.log(("My mum's age is " + ageOfMum) + (" and my dad's age is " + ageOfMum * 1.2));
+// }
+// userInfo()
+
+// // //class exrcise 2:
+// function moshe() {
+//     me = +prompt("age");
+//     mum = (me * 2);
+//     let result = mum
+
+// }
+// console.log(result);
+
+// const user = (username, password) {
+//     console.log()
+// }
