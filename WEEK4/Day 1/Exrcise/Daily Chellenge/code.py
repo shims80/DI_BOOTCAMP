0d9ec962399510a22784122b999d3e11eb75b3d7
shims80 <shims80@gmail.com>
2021-04-26 13:41:09 +0300

    daily challenge update

--- WEEK4/Day 1/Exrcise/Daily Chellenge/code.py
+++ WEEK4/Day 1/Exrcise/Daily Chellenge/code.py
@@ -1,15 +1,21 @@
 user =  input('please enter a word with 10 letters')
 if len(user) == 10:
     print (user[0],user[-1])
-        
+    target = ""
+    for letter in user:
+        target += letter 
+        print(target)
+    
 elif len(user) < 10:
     print('word not long enough')
 else:
     print('word too long')
 
-for i range:
-    for j in range(10, i + 1):
-        print(user)
+# word = 'helloworld'
+target = ""
+for letter in user:
+    target += letter 
+    print(target)
   
 
 
---
+++ WEEK4/Day 2/Exercise/Exercise in Class/code.py
@@ -0,0 +1,31 @@
+# list1 = [5, 10, 15, 20, 25, 50, 20]
+
+# wented_index = list1.index(20)
+# list1[3] = 200 
+# print(list1)
+
+
+# user_word = input("Enter a word which contains 10 characters : ")
+# letter_num = 10
+# def triangle_name(user_word):
+#     counter = 0
+#     constructed = ""
+#     if letter_num == len(user_word):
+#         for letter in user_word:
+#             constructed += letters
+#             print(constructed)
+#             counter +=1
+#             if counter == 10:
+#                 rand_word = list(user_word)
+#                 # [char for char in rand_word]
+#                 random.shuffle(rand_word)
+#                 shuffled = ''.join(rand_word)
+#                 print(shuffled)
+#     else:
+#         print("not enough letters")
+# triangle_name(user_word)
+
+numbers = 1
+while numbers <= 10:
+    numbers += 1
+print(numbers)
